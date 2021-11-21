b1d7479678801dedd3a2481cda95278e732173ab
杨旭彪 <yangxubiao-ls@bestpay.com.cn>
2021-11-21 15:40:09 +0800

    feat: 添加分油记录页接口

--- app/controllers/oilDivideRecord.js
+++ app/controllers/oilDivideRecord.js
@@ -1,10 +1,32 @@
 const OilDivideRecord = require('../models/oilDivideRecord')
 const { encryptToJava } = require('../utils/encrypt');
+const dayjs  = require('dayjs');
 
 class OilDivideRecordCtl {
   // 批量插入
   async insertMany(ctx) {
     const result = await OilDivideRecord.insertMany(ctx.request.body.data);
+    ctx.body = encryptToJava(JSON.stringify({
+      success: true,
+      errorMas: '',
+      errorCode: '',
+      result,
+    }));
+  }
+  async getOilDivideRecord(ctx) {
+    const { per_page = 10, query_page = 1 } = ctx.query;
+    const page = Math.max(query_page * 1, 1) - 1;
+    const perPage = Math.max(per_page * 1, 1);
+    const {
+      time,
+      ...rest 
+    } = ctx.request.body;
+    const startTime = dayjs(dayjs(time).format('YYYY-MM')).toDate();
+    const endTime = dayjs(dayjs(time).add(1, 'month').format('YYYY-MM')).toDate();
+    const result = await OilDivideRecord.find({
+      ...rest,
+      createdAt: {$gte: startTime, $lte: endTime} 
+    }).limit(perPage).skip(page * perPage).sort({ createdAt : -1 });
     ctx.body = encryptToJava(JSON.stringify({
       success: true,
       errorMas: '',
--- app/routes/oilDivideRecord.js
+++ app/routes/oilDivideRecord.js
@@ -1,9 +1,12 @@
 const Router = require('koa-router');
 const router = new Router({prefix: '/oilDivideRecord'});
 const { 
-  insertMany
+  insertMany,
+  getOilDivideRecord,
 } = require('../controllers/oilDivideRecord');
 
 router.post('/insertMany', insertMany)
+
+router.post('/getOilDivideRecord', getOilDivideRecord)
 
 module.exports = router;
